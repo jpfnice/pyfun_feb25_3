8a8a3f2ae07da841a6c13ca3bedad1b0c6defe7e
jpfnice <jpfnice@gmail.com>
2025-02-06 15:47:15 +0100

    Add files via upload

--- exercise10.py
+++ exercise10.py
@@ -23,9 +23,39 @@ Name of the attributes to provide:
     content -> list
     
 """
-
+class Stack:
+    def push(self,element):
+        pass
+    def pop(self):
+        pass
+    def peek(self) :
+        pass
+    def __len__(self) :
+        pass
+    def __repr__(self) :
+        pass
+    def  __contains__(self) :
+        pass
+    def __init__(self, msize) :
+        if isinstance(msize, int) and msize > 0:
+            self.maxSize=msize
+        else:
+            raise Exception("Wrong size given!")
+        self.content=[] # <=> self.content=list()
+        
+    
 s1=Stack(12) # 12 is the maxmimum size of the Stack s1
-
-# d=[7,8,8]
-# print(len(d)) # print(d.__len__())
+s1.push(10)
+s1.push(8)
+s1.push(67)
+print(s1) # (3/12) [10,8,67]
+print("Size of the stack", len(s1)) # Size of the stack 3
+top=s1.pop()
+print(s1) # (2/12) [10,8]
+print("Size of the stack", len(s1)) # Size of the stack 2
+print(top) # 67
+top=s1.peek()
+print(s1) # (2/12) [10,8]
+print("Size of the stack", len(s1)) # Size of the stack 2
+print(top) # 8
 
